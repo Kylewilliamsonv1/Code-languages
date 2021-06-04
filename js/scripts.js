a65ac0ca16fcf26575cead2033c2cbc7eaf2d44f
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-04 12:04:53 -0700

    create 2nd variable pick2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
   $("#surveyQuestions").submit(function(event) {
-    event.preventDefault();
-    const language = $("#survey1").val();
-    if (language==="frontend") {
+    const pick1 = $("#survey1").val();
+    const pick2 = $("#survey2").val();
+    if (pick1==="frontend" ) {
       $("#selections").show();
+      $(".selectionA").show();
+    } else if (pick1==="backend") {
+      $("#selections").show();
+      $(".selectionE").show();
+    } else {
+      $(".selectionG").show();  
     }
+    if (pick2==="q2socialmedia" ) {
+      $("#selections").show();
+      $(".selectionA").show();
+    } else if (pick2==="q2videogames") {
+      $("#selections").show();
+      $(".selectionE").show();
+    } else {
+      $(".selectionG").show();  
+    }
+
+    event.preventDefault();
   });
 });
