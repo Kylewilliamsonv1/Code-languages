2e956fcbecaa39923d939cac0cecb1ce293cea58
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-04 13:10:02 -0700

    add 3 variables to js page

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $("#surveyQuestions").submit(function(event) {
     const pick1 = $("#survey1").val();
     const pick2 = $("#survey2").val();
+    const pick3 = $("#survey3").val();
+    const pick4 = $("#survey4").val();
+    const pick5 = $("#survey5").val()
     if (pick1==="frontend" ) {
       $("#selections").show();
       $(".selectionA").show();
@@ -15,6 +18,33 @@ $(document).ready(function() {
       $("#selections").show();
       $(".selectionA").show();
     } else if (pick2==="q2videogames") {
+      $("#selections").show();
+      $(".selectionE").show();
+    } else {
+      $(".selectionG").show();  
+    }
+    if (pick3==="q3yes" ) {
+      $("#selections").show();
+      $(".selectionB").show();
+    } else if (pick3==="q3no") {
+      $("#selections").show();
+      $(".selectionE").show();
+    } else {
+      $(".selectionG").show();  
+    }
+    if (pick4==="q4coffee" ) {
+      $("#selections").show();
+      $(".selectionC").show();
+    } else if (pick4==="q4tea") {
+      $("#selections").show();
+      $(".selectionD").show();
+    } else {
+      $(".selectionG").show();  
+    }
+    if (pick5==="q5mac" ) {
+      $("#selections").show();
+      $(".selectionA").show();
+    } else if (pick5==="q5windows") {
       $("#selections").show();
       $(".selectionE").show();
     } else {
